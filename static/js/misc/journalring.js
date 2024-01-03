8061e9bf97ed118b3f55132c2475df489958be35
rio <96816712+js044@users.noreply.github.com>
2024-01-03 12:43:45 -0400

    slight css journalring update

--- static/js/misc/journalring.js
+++ static/js/misc/journalring.js
@@ -6,7 +6,7 @@ class JournalRing extends HTMLElement {
         // Define default styles for the widget
         this.defaultStyles = `
             .widget-container {                
-                max-width: 12em;
+                max-width: 100%;
                 display: flex;
                 flex-direction: column;
                 align-items: center;
